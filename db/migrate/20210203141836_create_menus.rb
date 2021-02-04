e4f7de86668709e5af385b708e7d4caf1f679edd
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-04 17:52:01 +0900

    ユーザーログイン・新規登録のフロント実装

--- db/migrate/20210203141836_create_menus.rb
+++ db/migrate/20210203141836_create_menus.rb
@@ -3,7 +3,7 @@ class CreateMenus < ActiveRecord::Migration[6.0]
     create_table :menus do |t|
       t.string :title,          null: false
       t.text :recipe,           null: false
-      t.references :user,       null: false, foreign_key: true
+      t.references :user,       foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_141836) do
   create_table "menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "recipe", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_menus_on_user_id"
